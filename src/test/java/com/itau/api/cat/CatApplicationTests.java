272e4d04340f386fd211f7a8404df579b60b0174
Joyce <joycemelo@ciandt.com>
2022-05-13 21:48:24 -0300

    inicializando project

---
+++ src/test/java/com/itau/api/cat/CatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itau.api.cat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
